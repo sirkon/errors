1e49ea278ddc1f0ff3e9499872e37c5abefcdd45
Денис Черемисов <d.cheremisov@vkteam.ru>
2026-02-14 18:33:09 +0300

    feat: native slog support

--- internal/example/OUTPUT.md
+++ internal/example/OUTPUT.md
@@ -1,18 +1,48 @@
-# Пример выхлопа логирования.
+# Logging output example.
 
-## Log with all context from errors shown all together.
+## Error context grouped by places where it was added.
 ```json
 {
-  "time": "2025-10-03T02:56:10.326348+03:00",
+  "time": "2026-02-14T18:31:43.766419+03:00",
   "level": "INFO",
   "source": {
-    "function": "main.LogFlat",
-    "file": "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go",
-    "line": 100
+    "function": "main.main",
+    "file": "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go",
+    "line": 47
   },
-  "msg": "logging test with flat output",
-  "int-value": 12,
-  "string-value": "hello",
+  "msg": "hello world",
+  "err": "ask to do something: failed to do something",
+  "@err": {
+    "CTX": {
+      "@location": "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:27",
+      "pi": 3.141592653589793,
+      "e": 2.718281828459045
+    },
+    "WRAP: ask to do something": {
+      "@location": "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:24",
+      "insert-locations": true
+    },
+    "NEW: failed to do something": {
+      "@location": "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:19",
+      "int-value": 13,
+      "string-value": "world"
+    }
+  },
+  "name": "value"
+}
+```
+
+## Flat structure, error context is shown all together.
+```json
+{
+  "time": "2026-02-14T18:31:43.766713+03:00",
+  "level": "INFO",
+  "source": {
+    "function": "main.main",
+    "file": "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go",
+    "line": 60
+  },
+  "msg": "hello world",
   "err": "ask to do something: failed to do something",
   "@err": {
     "pi": 3.141592653589793,
@@ -21,45 +51,11 @@
     "int-value": 13,
     "string-value": "world",
     "@locations": {
-      "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:49": "CTX",
-      "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:46": "WRAP: ask to do something",
-      "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:41": "NEW: failed to do something"
+      "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:27": "CTX",
+      "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:24": "WRAP: ask to do something",
+      "/Users/d.cheremisov/Sources/mine/errors/internal/example/main.go:19": "NEW: failed to do something"
     }
   },
-  "x": 1.5,
-  "err-naked": "naked error message"
-}
-```
-
-## Log with error context grouped by the places it was added.
-```json
-{
-  "time": "2025-10-03T02:56:10.326488+03:00",
-  "level": "INFO",
-  "source": {
-    "function": "main.LogGrouped",
-    "file": "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go",
-    "line": 106
-  },
-  "msg": "logging test with error context grouped by the places it was added",
-  "grouped-structure": true,
-  "err": "ask to do something: failed to do something",
-  "@err": {
-    "CTX": {
-      "@location": "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:49",
-      "pi": 3.141592653589793,
-      "e": 2.718281828459045
-    },
-    "WRAP: ask to do something": {
-      "@location": "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:46",
-      "insert-locations": true
-    },
-    "NEW: failed to do something": {
-      "@location": "/Users/d.cheremisov/Sources/work/errors/internal/example/main.go:41",
-      "int-value": 13,
-      "string-value": "world"
-    }
-  },
-  "greeting": "Hello, World!"
+  "name": "value"
 }
 ```
--- internal/example/main.go
+++ internal/example/main.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"go/token"
 	"io"
 	"log/slog"
 	"math"
@@ -14,28 +13,7 @@ import (
 )
 
 func main() {
-	// Output "prettily". In general, it would be correct to do this at the
-	// slog.Handler level, but in our case this is sufficient.
-	//
-	// At the same time, prepare to save the output to the OUTPUT.md file to avoid manually copying
-	// the new output after any changes.
-	w := &prettyJSONWriter{
-		collect: bytes.NewBuffer(nil),
-	}
-
-	// Reconfigure slog for convenient human-readable output.
-	slog.SetDefault(slog.New(slog.NewJSONHandler(
-		w,
-		&slog.HandlerOptions{
-			AddSource:   true,
-			Level:       nil,
-			ReplaceAttr: nil,
-		},
-	)))
 	errors.InsertLocations()
-
-	//
-	// Create an error for logging.
 
 	// Create a new error.
 	err := errors.New("failed to do something").
@@ -48,36 +26,46 @@ func main() {
 	// Add additional structured context.
 	err = errors.Just(err).Flt64("pi", math.Pi).Flt64("e", math.Exp(1))
 
-	// Log with logging context and error context.
-	LogFlat(
-		"logging test with flat output",
-		slog.Int("int-value", 12),
-		slog.String("string-value", "hello"),
-		errors.Log(err), // Same as slog.Any("err", err).
-		slog.Float64("x", 1.5),
-		slog.Any("err-naked", errors.Const("naked error message")),
-	)
+	// Output "prettily". In general, it would be correct to do this at the
+	// slog.Handler level, but in our case this is sufficient.
+	//
+	// At the same time, prepare to save the output to the OUTPUT.md file to avoid manually copying
+	// the new output after any changes.
+	w := &prettyJSONWriter{
+		collect: bytes.NewBuffer(nil),
+	}
 
-	// Save the first output example for OUTPUT.md
+	// Reconfigure slog for convenient human-readable output.
+	slog.SetDefault(slog.New(errors.NewSLogErrorContextGrouppedHandler(slog.NewJSONHandler(
+		w,
+		&slog.HandlerOptions{
+			AddSource:   true,
+			Level:       nil,
+			ReplaceAttr: nil,
+		},
+	))))
+	slog.Info("hello world", err, "name", "value")
 	md1 := w.collect.String()
-	w.collect.Reset()
-
-	// Second example, error context will be grouped by processing location.
-	LogGrouped(
-		"logging test with error context grouped around places it was added",
-		slog.Bool("grouped-structure", true),
-		errors.Log(err),
-		slog.String("greeting", "Hello, World!"),
-	)
 
 	// Generate OUTPUT.md content
+	w.collect.Reset()
+	slog.SetDefault(slog.New(errors.NewSLogErrorContextFlatHandler(slog.NewJSONHandler(
+		w,
+		&slog.HandlerOptions{
+			AddSource:   true,
+			Level:       nil,
+			ReplaceAttr: nil,
+		},
+	))))
+	slog.Info("hello world", err, "name", "value")
 	md2 := w.collect.String()
+
 	mdFormat := `# Logging output example.
 
-## Flat structure, error context is shown all together.
+## Error context grouped by places where it was added.
 %s
 
-## Error context grouped by places where it was added.
+## Flat structure, error context is shown all together.
 %s
 `
 	md := fmt.Sprintf(mdFormat, mdJSON(md1), mdJSON(md2))
@@ -91,19 +79,6 @@ func main() {
 
 func mdJSON(v string) string {
 	return fmt.Sprintf("```json\n%s```", v)
-}
-
-// LogFlat function for logging to slog at Info level with extraction of context from error objects
-// received in fields. Uses "flat" error context structure.
-func LogFlat(msg string, fields ...slog.Attr) {
-	attrs := feedFieldsWithFlatErrorContext(fields)
-	slog.Info(msg, attrs...)
-}
-
-// LogGrouped similar to LogFlat, but uses error structure separated by processing location.
-func LogGrouped(msg string, fields ...slog.Attr) {
-	attrs := feedFieldsWithGroupedErrorContext(fields)
-	slog.Info(msg, attrs...)
 }
 
 // Special implementation of [io.Writer] for outputting JSON in a human-readable format.
@@ -124,121 +99,4 @@ func (w *prettyJSONWriter) Write(p []byte) (n int, err error) {
 	}
 
 	return int(written), nil
-}
-
-const (
-	locationsLoggingKey = "@locations"
-)
-
-func errContextKey(errname string) string {
-	return "@" + errname
-}
-
-type Logger struct {
-}
-
-// This function modifies the list of fields, adding error context if present.
-func feedFieldsWithFlatErrorContext(fields []slog.Attr) []any {
-	const contextLengthPrediction = 4
-
-	attrs := make([]any, 0, len(fields)+contextLengthPrediction)
-	for _, field := range fields {
-		// Save all original fields preserving their order.
-		attrs = append(attrs, field)
-
-		// Separately process fields containing errors.
-		// Errors, as "complex" types, are contained in Any().
-		fv := field.Value.Any()
-		if fv == nil {
-			continue
-		}
-		e, ok := fv.(error)
-		if !ok {
-			continue
-		}
-
-		// Try to extract context.
-		d := errors.GetContextDeliverer(e)
-		if d == nil {
-			// This is not our error.
-			continue
-		}
-
-		// Create a container for the extracted context and fill it with values.
-		// Then add the accumulated values to the list of fields for logging
-		// as part of the @<err-key> group.
-		cons := flatConsumer{
-			fields: make([]any, 0, contextLengthPrediction),
-		}
-		d.Deliver(&cons)
-
-		// Create the content of the @location group to display error processing locations
-		// with descriptions of the processing method.
-		var locs []any
-		for _, loc := range cons.locs {
-			if !loc.IsValid() {
-				continue
-			}
-			locs = append(locs, slog.String(loc.String(), loc.Descr))
-		}
-		if len(locs) > 0 {
-			cons.fields = append(cons.fields, slog.Group(locationsLoggingKey, locs...))
-		}
-
-		attrs = append(attrs, slog.Group(errContextKey(field.Key), cons.fields...))
-	}
-
-	return attrs
-}
-
-// This function modifies the list of fields, adding error context if present, grouping it by
-// processing location.
-func feedFieldsWithGroupedErrorContext(fields []slog.Attr) []any {
-	const contextLengthPrediction = 4
-
-	attrs := make([]any, 0, len(fields)+contextLengthPrediction)
-	for _, field := range fields {
-		// Save all original fields preserving their order.
-		attrs = append(attrs, field)
-
-		// Separately process fields containing errors.
-		// Errors, as "complex" types, are contained in Any().
-		fv := field.Value.Any()
-		if fv == nil {
-			continue
-		}
-		e, ok := fv.(error)
-		if !ok {
-			continue
-		}
-
-		// Try to extract context.
-		d := errors.GetContextDeliverer(e)
-		if d == nil {
-			// This is not our error.
-			continue
-		}
-
-		cons := newGroupedConsumer()
-		d.Deliver(cons)
-
-		errCtxFields := make([]any, 0, contextLengthPrediction)
-		for _, c := range cons.consumers {
-			fs := make([]any, 0, contextLengthPrediction)
-			if c.loc.IsValid() {
-				fs = append(fs, slog.String("@location", c.loc.String()))
-			}
-			fs = append(fs, c.fields...)
-			errCtxFields = append(errCtxFields, slog.Group(c.text, fs...))
-		}
-
-		attrs = append(attrs, slog.Group(errContextKey(field.Key), errCtxFields...))
-	}
-
-	return attrs
-}
-
-type annotatedPosition struct {
-	token.Position
-	Descr string
 }
